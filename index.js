a4dea65f7ca3ffaa82bcc3d01e2fb3f881bd8400
Abnnerr <jonnas.ab.00@gmail.com>
2025-05-05 07:49:31 -0300

    Update

--- index.js
+++ index.js
@@ -1,26 +1,48 @@
 function showLay() {
     let overlayback = document.querySelector('#overlay');
     let gavetaElemente = document.querySelector('#gaveta')
-    if(overlayback.classList.contains('invisible')) {
-        overlayback.classList.remove('invisible','opacity-0')
+    let editarContei = document.querySelector('#conteiEdit')
+
+    if (overlayback.classList.contains('invisible')) {
+
+        overlayback.classList.remove('invisible', 'opacity-0')
         gavetaElemente.classList.remove("-right-full")
         gavetaElemente.classList.add('right-0')
+
     } else {
-        overlayback.classList.add('invisible','opacity-0')
+
+        overlayback.classList.add('invisible', 'opacity-0')
         gavetaElemente.classList.add("-right-full")
         gavetaElemente.classList.remove('right-0')
+
+        editarContei.classList.add('-right-full')
+        editarContei.classList.remove('right-0')
     }
 }
-function closeAll(){
+function closeAll() {
     let overlayback = document.querySelector('#overlay');
     let gavetaElemente = document.querySelector('#gaveta')
-    if(overlayback.classList.contains('invisible')) {
-        overlayback.classList.remove('invisible','opacity-0')
+    let editarContei = document.querySelector('#conteiEdit')
+    
+    if (overlayback.classList.contains('invisible')) {
+        overlayback.classList.remove('invisible', 'opacity-0')
         gavetaElemente.classList.remove("-right-full")
         gavetaElemente.classList.add('right-0')
     } else {
-        overlayback.classList.add('invisible','opacity-0')
+        overlayback.classList.add('invisible', 'opacity-0')
+
         gavetaElemente.classList.add("-right-full")
         gavetaElemente.classList.remove('right-0')
+
+        editarContei.classList.add('-right-full')
+        editarContei.classList.remove('right-0')
     }
+}
+function editar() {
+    let overlayback = document.querySelector('#overlay');
+    let editarContei = document.querySelector('#conteiEdit')
+    overlayback.classList.remove('invisible', 'opacity-0')
+    editarContei.classList.remove('-right-full')
+    editarContei.classList.add('right-0')
+
 }
