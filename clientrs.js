2cf297c62795aa716da8a31ab5ffb67cb47dc13c
Abnnerr <jonnas.ab.00@gmail.com>
2025-05-05 16:59:19 -0300

    update

---
+++ clientrs.js
@@ -0,0 +1,5 @@
+let clientes = {
+    nome:' conrado',
+    email:'conrador@gmail.com',
+    
+}
---
+++ index2.js
@@ -0,0 +1,17 @@
+// function moveZeros(arr) {
+//     let lista= []
+
+//     for (let i = 0; i < arr.length; i++) {
+
+//         if ((arr[i] != 0)) {
+//             lista.push(arr[i])
+
+//         }
+//         if ((arr[i] == 0)) {
+//             lista.push(arr[i])
+//         }
+
+//     }
+//     return lista
+// }
+// console.log(moveZeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1]))
