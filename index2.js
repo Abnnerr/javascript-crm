a7b1171449980ef533aeb76edb7e4e64a6a5ca56
unknown <jonasvieira0891@digitalcollege.internal>
2025-05-07 16:54:57 -0300

    update

--- index2.js
+++ index2.js
@@ -17,9 +17,27 @@
 // console.log(moveZeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1]))
 
 
+function findOdd(A) {
 
-function findUniq(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        console.log(arr[i])
+    var listaPar = []
+    contadorPar = 0
+    contadorImpar = 0
+    for (var i = 0; i < A.length; i++) {
+        // console.log(A[i])
+
+        if (A[i] % 2 == 0) {
+            contadorPar += 1
+            listaPar.push(A[i])
+            // console.log(listaPar)
+
+        }
+        if (A[i] % 2 != 0) {
+            contadorImpar += 1
+
+        }
+    }
+
+    // console.log(contadorPar)
+    // console.log(contadorImpar)
 }
-findUniq([ 1, 0, 0 ])
+findOdd([1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1])
--- js/clientes.js
+++ js/clientes.js
@@ -1,13 +1,45 @@
 
-let clientes = JSON.parse(sessionStorage.getItem("clientes"));
+let clientes = []
+
+
+function buscarClientes() {
+    fetch("http://localhost:3000/clientes")
+        .then((response) => response.json())
+        .then((lista) => {
+            clientes = lista;
+            carregarClientes(clientes)
+        })
+}
+buscarClientes();
+
+function carregarClientes(listaDeClientes) {
+    let tbodyElement = document.querySelector('#tabela')
+    tbodyElement.innerHTML = '';
+    listaDeClientes.map((cliente) => {
+        tbodyElement.innerHTML += `<tr class="text-[#464646]">
+        <td class="">1</td>
+        <td>${cliente.nome}</td>
+        <td>${cliente.email}</td>
+        <td>${cliente.tel}</td>
+        <td>${cliente.data}</td>
+        <td>Ativo</td>
+        <td class="flex justify-center items-center py-[20px]  gap-[5px]">
+        <box-icon type='solid' name='pencil'
+        class="fill-pink-600 p-2 w-[40px] h-[40px] rounded-full cursor-pointer hover:bg-pink-600 hover:fill-white duration-[600ms]"
+        onclick="editar()"></box-icon>
+        <box-icon type='solid' name='trash'
+        class="fill-pink-600 p-2 w-[40px] h-[40px] rounded-full cursor-pointer hover:bg-pink-600 hover:fill-white duration-[600ms]"></box-icon>
+        </td>
+        </tr>`;
+    })
+}
+
 
 function criarCliente() {
     event.preventDefault()
 
     // let formData = new FormData(form)
     // let cliente = Object.fromEntries(formData.entries())
-
-
 
     let nameValue = document.querySelector('#nomeCliente').value;
     let emailValue = document.querySelector('#emailCliente').value;
@@ -20,18 +52,7 @@ function criarCliente() {
     if (clienteJaExiste) {
         alert('Já existe');
     }
-    // else if (clienteJaExisteEmail) {
-    //     alert('Já existe');
-    // }
-    // else if (clienteJaExisteTel) {
-    //     alert('Já existe');
-    // }
-    // else if (clienteJaExisteData) {
-    //     alert('Já existe');
-    // }
-    // else if(nameValue === '' && emailValue == '') {
-    //     alert('erro')
-    // }
+
     else {
         clientes.push({
             nome: nameValue,
@@ -39,47 +60,10 @@ function criarCliente() {
             tel: telValue,
             data: dataValue,
         })
-        //     cliente = document.querySelector("#tabela");
-        //     cliente.innerHTML += `  <tr class="text-[#464646]">
-        //                         <td class="">1</td>
-        //                         <td>${nameValue}</td>
-        //                         <td>${emailValue}</td>
-        //                         <td>${email}</td>
-        //                         <td>telefone</td>
-        //                         <td>Ativo</td>
-        //                         <td class="flex justify-center items-center py-[20px]  gap-[5px]">
-        //                             <box-icon type='solid' name='pencil'
-        //                                 class="fill-pink-600 p-2 w-[40px] h-[40px] rounded-full cursor-pointer hover:bg-pink-600 hover:fill-white duration-[600ms]"
-        //                                 onclick="editar()"></box-icon>
-        //                             <box-icon type='solid' name='trash'
-        //                                 class="fill-pink-600 p-2 w-[40px] h-[40px] rounded-full cursor-pointer hover:bg-pink-600 hover:fill-white duration-[600ms]"></box-icon>
-        //                         </td>
-        //                     </tr>`
-        // };
-        carregarClientes(clientes)
+        sessionStorage.setItem('clientes', JSON.stringify(clientes))
         closeAll()
+
     }
 
 
-    function carregarClientes(listaDeClientes) {
-        let tbodyElement = document.querySelector('#tabela')
-        tbodyElement.innerHTML = '';
-        listaDeClientes.map((cliente) => {
-            tbodyElement.innerHTML += `<tr class="text-[#464646]">
-                            <td class="">1</td>
-                            <td>${cliente.nome}</td>
-                            <td>${cliente.email}</td>
-                            <td>${cliente.tel}</td>
-                            <td>${cliente.data}</td>
-                            <td>Ativo</td>
-                            <td class="flex justify-center items-center py-[20px]  gap-[5px]">
-                                <box-icon type='solid' name='pencil'
-                                    class="fill-pink-600 p-2 w-[40px] h-[40px] rounded-full cursor-pointer hover:bg-pink-600 hover:fill-white duration-[600ms]"
-                                    onclick="editar()"></box-icon>
-                                <box-icon type='solid' name='trash'
-                                    class="fill-pink-600 p-2 w-[40px] h-[40px] rounded-full cursor-pointer hover:bg-pink-600 hover:fill-white duration-[600ms]"></box-icon>
-                            </td>
-                        </tr>`;
-        })
-    }
 }
